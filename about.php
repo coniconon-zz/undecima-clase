8c04bf796099bf2981bcdd7216cedae0cafcc444
Constanza Morales <coniconon@users.noreply.github.com>
2017-05-24 15:34:42 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include('header.php')?>
+<div class="row">
+<div class="col-sm-12">
+<h3>Sobre el proyecto</h3>
+<p>Este proyecto se desarrolla como evaluación para la clase de Diseño y Nuevos Medios de la Universidad Católica.
+El objetivo es hacer un sitio web con algún contenido de interés propio, que en mi caso es la visualización de datos.</p>
+<p>Para ello, quiero enfocarme en crear un sitio donde la gente pueda encontrar proyectos de visualización que resulten interesantes, desarrollados particularmente en Chile. Esto para crear difusión sobre el tema en el país, que muchas veces pasa desapercibido o no recibe atención de los medios.</p>
+<p>Actualmente me encuentro haciendo mi proyecto de título para optar al grado de Diseñadora, y mi foco de investigación me ha llevado a tener contacto con el medio local, donde he podido conocer a las personas detrás de estos proyectos, así como hacer un "catastro" del panorama de la visualización de datos en el país</p>
+</div><!--/col-sm-12-->
+</div><!--/row->
+<?php include('footer.php')?>
